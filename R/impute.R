a3b4afefbb310dd3bf7d6ff84c44bf1bc74e5dea
Nathan Morris <morris_nathan@lilly.com>
2021-01-16 14:18:46 -0500

    Remove unused options to reduce validation burden.

--- R/impute.R
+++ R/impute.R
@@ -3,119 +3,105 @@
 #' @seealso \code{\link{bootstrap_param_est}}
 #' @export
 #' @param wide Data in wide format (i.e., each day is a column). 
-#' Data must be in extended representation. Here as state of (9) is missing and possibly dead, and 
-#' a state of (10) is missing but not dead.
-#' @param bin The assigned bin for each transition. Must be a numeric vector of length=(1-length(days)).
 #' @param m Number of imputations.
 #' @param by Character vector with column names. Data will be broken up and imputed separately for every combination of values for those columns in the data.
-#' @param baselineStrat Character vector with column names. Baseline proportion estimated separately for each strata.
+#' @param days Names of the columns that contain the score for each day. Columns should be in sequential order.
+#' @param bin The assigned bin for pooling together information across transitions. Must be a numeric vector of length=(length(days)-1). By defualt all transitions are pooled together.
 #' @param Em Emission probabilities. Generally the default should not be changed.
-#' @param days Names of the columns that contain the score for each day.
-#' @param wideFormatOut Return in wide format (TRUE) or long format (FALSE)
 #' @param listFormatOut Return each imputed dataset in a list or combine into a single dataset.
 #' @param tol tolerance for relative reduction the log-likelihood to determine convergence of the Baum-Welch algorythm.
 #' @param silent Allows silencing some messages.
-#' @param s For internal use only.
 #' @details
-#' 
-#' @return 
-#' A list object with the following components
-#' \describe{
-#'   \item{fit}{Contains results of the primary model fit}
-#'   \item{boot}{Contains relults from the bootstrap model fit.}
-#'   \item{bin}{The input.}
-#'   \item{days}{Formatted dataset.}
-#'   \item{Em}{Attempted target.}
+#' States for each patient/day in 'wide' may be the following: 
+#' \itemize{
+#'  \item{Not missing:}{An integer from 1 to 8.}
+#'  \item{Missing:}{NA}
+#'  \item{Partially Missing:}{ range which may be code as a characters string such as '[1,7]' or '[1,2]'. Such a character string indicates that while the actual value is unknown, it is known that the value falls within the specified range. }
 #' }
+#' @return 
+#' If listFormatOut = TRUE, then a list will be returned with each element being an imputed data set.
+#' If listFormatOut = FALSE, then a single data.frame will be returned where IMP_ID column is created.
 #' 
 #' 
 #' @examples
 impute <-
-function(wide, bin, m, by=NULL, baselineStrat=NULL, days=paste0("D",1:28),
-                  Em=get_emission(wide, days), wideFormatOut=TRUE,
-                  listFormatOut=TRUE, tol=1E-6, silent=FALSE, s=NULL) {
-  
-  #Setup up baseline starta assigning each patient to a strata
-  if(is.null(baselineStrat)) {
-    s=rep(1, nrow(wide))
-  } else {
-    if(!is.null(s))
-      stop("Cannot specify both 'baselineStrat' and 's.'")
-    if(!is.character(baselineStrat))
-      stop("'baselineStrat' must be of type character.")
-    if(!all(baselineStrat %in% colnames(wide)))
-      stop("All variables listed in by must be in the 'wide' data.frame.")
-    if(is.null(by))
-      if(any(baselineStrat %in% by))
-        stop("Baseline stratification factors cannot be included as a 'by' grouping.")
-    tmp=by_str_for_impute=apply(wide[,baselineStrat], 1, function(X) paste(X,collapse="|"))
-    s=as.numeric(as.factor(tmp))
-  }
-
-  
-  if(is.null(by)) { #no by grouping so just go
-    fit=bootstrap_param_est(wide=wide,  s=s, bin=bin, b=m,
-                            days=days, Em=Em,tol=tol, silent=silent)
+  function(wide, m, by=NULL, days=paste0("D",1:28), bin=rep(1,length(days)-1), 
+           Em=get_emission(wide, days),
+           listFormatOut=FALSE, tol=1E-6, maxiter=200, silent=FALSE) {
+    if(!is.numeric(bin))
+      stop("bin must be numeric.")
+    if(!is.vector(bin))
+      stop("bin must be a vector")
+    if(length(bin)!=(length(days)-1))
+      stop("length(bin) must be the same as length(days)-1.")
     
-    imp=lapply(fit$boot, function(bfit) {
-      .impute_one(Pri=bfit$Pri, s=s, Tran=bfit$Tran, 
-                 Em=Em, 
-                 bin=bin)
-    })
-    extravars=wide[,which(!(colnames(wide) %in% days))]
-    if(wideFormatOut) {
+    if(!is.numeric(m))
+      stop("m must be numeric.")
+    if(!is.vector(m))
+      stop("m must be a vector")
+    if(length(m)!=1)
+      stop("length(m) must be 1.")
+    if(m<2)
+      stop("m must be >=2.")
+    
+    #originally I implemented to stratify baseline. However, not planning to expose this functionality.
+    #the following line sets up for no strata
+    s=rep(1, nrow(wide))
+    
+    
+    if(is.null(by)) { #no by grouping so just go
+      fit=bootstrap_param_est(wide=wide, b=m, bin=bin,
+                              days=days, Em=Em,tol=tol,  
+                              maxiter=maxiter, silent=silent)
+      
+      imp=lapply(fit$boot, function(bfit) {
+        .impute_one(Pri=bfit$Pri, s=s, Tran=bfit$Tran, 
+                    Em=Em, 
+                    bin=bin)
+      })
+      extravars=wide[,which(!(colnames(wide) %in% days))]
+      
       ret=mapply(function(impi,i) {
         colnames(impi)=days
         data.frame(IMP_ID=i,extravars,impi)
       }, imp, 1:length(imp), SIMPLIFY = FALSE)
+      
     } else {
-      orig=melt(t(as.matrix(wide[,days])))
-      ret=mapply(function(impi,i) {  
-        colnames(impi)=days
-        rownames(impi)=1:nrow(impi)
-        impi=melt(t(impi))
-        names(impi)=c("DAY", "PATIENT", "value")
-        impi$orig_value=orig[,3]
-        data.frame(IMP_ID=i,extravars[imp$PATIENT,],imp)
-      }, imp, 1:length(imp), SIMPLIFY = FALSE)
+      
+      if(!is.character(by))
+        stop("'by' must be of type character.")
+      if(!all(by %in% colnames(wide)))
+        stop("All variables listed in by must be in the 'wide' data.frame.")
+      
+      by_str_for_impute=apply(wide[,by, drop=FALSE], 1, function(X) paste(X,collapse="|"))
+      
+      poslst=split(1:nrow(wide),by_str_for_impute)
+      imp=lapply(poslst, function(pos) {
+        # browser()
+        wide_by=wide[pos,]
+        s_by=s[pos]
+        Em_by=Em[pos,,]
+        impute(wide=wide_by, bin=bin, m=m, 
+               days=days, Em=Em_by, 
+               listFormatOut=TRUE, 
+               by=NULL, silent=silent)
+      })
+      
+      ret=list()
+      for(i in 1:m)
+        ret[[i]]=do.call(rbind, lapply(imp,function(x) x[[i]] ))
+      
+      ret=lapply(ret, function(x) {
+        rownames(x)=NULL
+        return(x)
+      })
+    }     
+    if(!listFormatOut) {
+      ret=do.call(rbind, ret)
     }
-  } else {
-
-    if(!is.character(by))
-      stop("'by' must be of type character.")
-    if(!all(by %in% colnames(wide)))
-      stop("All variables listed in by must be in the 'wide' data.frame.")
+    return(ret)
     
-    by_str_for_impute=apply(wide[,by], 1, function(X) paste(X,collapse="|"))
-
-    poslst=split(1:nrow(wide),by_str_for_impute)
-    imp=lapply(poslst, function(pos) {
-      # browser()
-      wide_by=wide[pos,]
-      s_by=s[pos]
-      Em_by=Em[pos,,]
-      impute(wide=wide_by,  s=s_by, bin=bin, m=m, 
-             days=days, Em=Em_by, 
-             wideFormatOut=wideFormatOut, listFormatOut=TRUE, 
-             by=NULL, silent=silent)
-    })
-
-    ret=list()
-    for(i in 1:m)
-      ret[[i]]=do.call(rbind, lapply(imp,function(x) x[[i]] ))
-    
-    ret=lapply(ret, function(x) {
-      rownames(x)=NULL
-      return(x)
-    })
   }
-  if(!listFormatOut) {
-    ret=do.call(rbind, ret)
-  }
-  return(ret)
-}
-
-
 
 
 .impute_one <-
