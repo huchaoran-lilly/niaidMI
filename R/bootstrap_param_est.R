735b337f48d6738f899098894d91ebf9c0f76a93
Nathan Morris <morris_nathan@lilly.com>
2021-02-15 17:49:46 -0500

    simplify unneeded check

--- R/bootstrap_param_est.R
+++ R/bootstrap_param_est.R
@@ -74,21 +74,21 @@ function(wide, b, days=paste0("D",1:28), bin=rep(1,length(days)-1),
   strt=.get_start(M=M,s=s,bin=bin)
   if(!silent) cat("Fiting Model\n")
   fit=.baum_welsh(Pri=strt$Pri, s=s, Tran=strt$Tran, Em=Em, bin=bin, tol=tol,  maxiter=maxiter)
-
+  
   ret=list(fit=fit, s=s, bin=bin, days=days, Em=Em)
-  if(b>=1) {
-    if(!silent) cat("Fiting Bootstrap Samples:\n")
-    ret$boot=lapply(1:b, function(i) {
-      if(!silent) cat("    b=",i)
-      boot=.bootstrap_dta(Em=Em, M=M, s=s)
-      # strt=.get_start(M=boot$M,s=boot$s, bin=bin)
-      
-      fit1=.baum_welsh(Pri=strt$Pri, s=boot$s, Tran=fit$Tran, Em=boot$Em, bin=bin, tol=tol,  maxiter=maxiter)
-      # fit1=.baum_welsh(Pri=strt$Pri, s=boot$s, Tran=fit$Tran, Em=boot$Em, bin=bin, tol=tol,  maxiter=maxiter)
-      if(!silent) cat(", iterations=", fit1$nit,"\n")
-      return(fit1[c("Tran","Pri")])
-    })
-  }
+  
+  if(!silent) cat("Fiting Bootstrap Samples:\n")
+  ret$boot=lapply(1:b, function(i) {
+    if(!silent) cat("    b=",i)
+    boot=.bootstrap_dta(Em=Em, M=M, s=s)
+    # strt=.get_start(M=boot$M,s=boot$s, bin=bin)
+    
+    fit1=.baum_welsh(Pri=strt$Pri, s=boot$s, Tran=fit$Tran, Em=boot$Em, bin=bin, tol=tol,  maxiter=maxiter)
+    # fit1=.baum_welsh(Pri=strt$Pri, s=boot$s, Tran=fit$Tran, Em=boot$Em, bin=bin, tol=tol,  maxiter=maxiter)
+    if(!silent) cat(", iterations=", fit1$nit,"\n")
+    return(fit1[c("Tran","Pri")])
+  })
+  
   return(ret)
 }
 
